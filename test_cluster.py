8c87f34de29f65c53ad44f1ecec7005e8121c503
Pedro Henrique Freitas Cabral/Mobile Product SW/Associate/Samsung Electronics <pedro.cabral@samsung.com>
2020-08-12 12:21:01 -0400

    - Added some comments - Optimized clustering method using medium points between cluster elements - Medium points plotted in graph

--- test_cluster.py
+++ test_cluster.py
@@ -4,9 +4,11 @@ import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 
-#data = [("A",20), ("B",50), ("C",75), ("D",25), ("E",25), ("F",78)]
+global pmVec
+pmVec=[(0,0),(0,0),(0,0)]
+
 data = [("A",5,5),("B",3,10),("C",5,12),("D",16,2),("E",15,9),("F",21,21),("G",22,20)]
-ga = galg.GeneticAlgorithm(data,20,500,0.8,0.4,True,True)
+ga = galg.GeneticAlgorithm(data,20,1000,1,0.4,True,True)
 
 def create_individual(data):
     numClusters=3
@@ -16,23 +18,37 @@ ga.create_individual = create_individual
 def fitness(individual, data):
     fn = 0
     numClusters=3
-    lstSomatorios = []
     for i in range(numClusters):
-        lsProfit=[]
+        #Pra cada cluster cria uma lista de elementos pertencentes apenas a ela
+        lsElements=[]
+        clusterEmpty=True
         for (selected, (_, x,y)) in zip(individual, data):
-            if(selected == i): lsProfit.append((x,y))
+            if(selected == i): 
+                lsElements.append((x,y))
+                clusterEmpty=False #Checa se existe alguma cluster vazia
+        if(clusterEmpty):
+            return 0 #Caso a cluster analisada esteja vazia retorna fitness 0
         
-        sm=0
-        for a in itertools.combinations(lsProfit,2):
-            A = a[0]
-            B = a[1]
-            sm+=pow(abs(pow(A[0]-B[0],2)+pow(A[1]-B[1],2)),0.5)
-        lstSomatorios.append(sm*len(lsProfit))
+        if(len(lsElements)==0): break
+        smCluster=0
+        pmX=0
+        pmY=0
+        clusterSize=len(lsElements)
+        #Define o ponto médio entre os elementos da cluster
+        for (x,y) in lsElements:
+            pmX+=x
+            pmY+=y
+        pmX/=clusterSize
+        pmY/=clusterSize
+        pmVec[i]=(pmX,pmY)
+        #Calcula a soma das distâncias dos pontos da cluster até o centro dela
+        for (x,y) in lsElements:
+            smCluster+=pow(pow(x-pmX,2)+pow(y-pmY,2),0.5)
+        #Calcula a média das distâncias (evita que clusters que tenham muitos elementos acabem com uma fitness reduzida indevidamente)
+        smCluster*=clusterSize
+        fn+=smCluster
+    fn=numClusters/fn
 
-    for p in lstSomatorios:
-        fn+= p 
-    
-    fn = numClusters/fn
     return fn
             
 ga.fitness_function = fitness
@@ -56,5 +72,7 @@ for clusterInd in range(numClusters):
             ax.plot(pt[1],pt[2],"*",c=colorset[clusterInd])
             ax.text(pt[1]-0.15,pt[2]+0.4,data[i][0])
         i+=1
+for (pmx,pmy) in pmVec:
+    ax.plot(pmx,pmy,".")
 
 plt.show()
